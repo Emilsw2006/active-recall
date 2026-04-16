061d174da04804dd4be6664cc851dd787dc4c541
Emilsw2006 <sagarraweikene@gmail.com>
2026-04-16 21:21:52 +0200

    ui: plan wizard fixed layout, session buttons higher, smoother VAD animation

--- TEST-APP/sw.js
+++ TEST-APP/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'active-recall-v33';
+const CACHE_NAME = 'active-recall-v34';
 const JS_CSS = /\.(js|css)(\?.*)?$/;
 
 self.addEventListener('install', () => self.skipWaiting());
@@ -44,6 +44,7 @@ self.addEventListener('fetch', e => {
     )
   );
 });
+
 
 
 
